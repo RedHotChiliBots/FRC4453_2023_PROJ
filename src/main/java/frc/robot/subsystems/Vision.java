c850c6c48dfe8745987c65e116f603ba95afae51
Steve Seifert <sseifert@redhotchilibots.com>
2023-01-26 22:29:43 -0500

    Update Vision

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -7,6 +7,7 @@ package frc.robot.subsystems;
 import java.util.List;
 
 import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 import org.photonvision.targeting.TargetCorner;
 
@@ -16,11 +17,45 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Vision extends SubsystemBase {
   /** Creates a new Vision. */
+
   // Change this to match the name of your camera
   PhotonCamera camera = new PhotonCamera("photonvision");
+
+  // Query the latest result from PhotonVision
+  PhotonPipelineResult result = null;
+
+  // Check if the latest result has any targets.
+  boolean hasTargets = false;
+
+  // Get a list of currently tracked targets.
+  List<PhotonTrackedTarget> targets = null;
+
+  // Get the current best target.
+  PhotonTrackedTarget target = null;
+
+  // Get information from target.
+  double yaw = 0.0;
+  double pitch = 0.0;
+  double area = 0.0;
+  double skew = 0.0;
+  Transform3d pose = null;
+  List<TargetCorner> corners = null;
+
+  // Get information from target.
+  int targetID = 0;
+  double poseAmbiguity = 0.0;
+  Transform3d bestCameraToTarget = null;
+  Transform3d alternateCameraToTarget = null;
+
+  double latencySeconds = 0.0;
+
   
   public Vision() {
+    // Set driver mode to on.
+    camera.setDriverMode(true);
 
+    // Change pipeline to 2
+    camera.setPipelineIndex(2);
   }
 
   @Override
@@ -28,29 +63,33 @@ public class Vision extends SubsystemBase {
     // This method will be called once per scheduler run
 
     // Query the latest result from PhotonVision
-    var result = camera.getLatestResult();
+    result = camera.getLatestResult();
+
+    // Get the pipeline latency.
+    latencySeconds = result.getLatencyMillis() / 1000.0;
 
     // Check if the latest result has any targets.
-    boolean hasTargets = result.hasTargets();
+    hasTargets = result.hasTargets();
 
     // Get a list of currently tracked targets.
-    List<PhotonTrackedTarget> targets = result.getTargets();
+    targets = result.getTargets();
 
     // Get the current best target.
-    PhotonTrackedTarget target = result.getBestTarget();
+    target = result.getBestTarget();
 
     // Get information from target.
-    double yaw = target.getYaw();
-    double pitch = target.getPitch();
-    double area = target.getArea();
-    double skew = target.getSkew();
-    Transform2d pose = target.getCameraToTarget();
-    List<TargetCorner> corners = target.getCorners();
+    yaw = target.getYaw();
+    pitch = target.getPitch();
+    area = target.getArea();
+    skew = target.getSkew();
+    pose = target.getBestCameraToTarget();
+    corners = target.getDetectedCorners();
 
     // Get information from target.
-    int targetID = target.getFiducialId();
-    double poseAmbiguity = target.getPoseAmbiguity();
-    Transform3d bestCameraToTarget = target.getBestCameraToTarget();
-    Transform3d alternateCameraToTarget = target.getAlternateCameraToTarget();
+    targetID = target.getFiducialId();
+    poseAmbiguity = target.getPoseAmbiguity();
+    bestCameraToTarget = target.getBestCameraToTarget();
+    alternateCameraToTarget = target.getAlternateCameraToTarget();
   }
+
 }

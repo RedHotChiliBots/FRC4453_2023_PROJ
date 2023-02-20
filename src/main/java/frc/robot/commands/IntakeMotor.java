ee96fe1f5aa6f11072aef6726ebbf2a61cf9b7be
Steve Seifert <sseifert@redhotchilibots.com>
2023-02-20 09:35:27 -0500

    Fix variable scope

--- src/main/java/frc/robot/commands/IntakeMotor.java
+++ src/main/java/frc/robot/commands/IntakeMotor.java
@@ -27,7 +27,7 @@ public class IntakeMotor extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    boolean oneTime = true;
+    oneTime = true;
     timer.start();
   }
 
